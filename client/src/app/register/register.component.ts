ba3c9410fb2dde0ea2098c165a149da95de32e79
RIchárd Kálmán <kalmanrichard112096@gmail.com>
2021-11-23 00:00:54 +0100

    routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = { };
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,9 +20,10 @@ export class RegisterComponent implements OnInit {
   register(){
     this.accountService.register(this.model).subscribe(response => {
       console.log(response);
+      this.toastr.error("error.error")
       this.cancel();
     }, error => {
-      console.log(error)
+      this.toastr.error(error.error)
     })
   }
 
